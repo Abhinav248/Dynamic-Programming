dd649380df062e47806fde4534193f085619e226
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-08-09 02:32:25 +0530

    Update 4_LongestIncreasingSubsequence.java
    Kadane's Algorithm to remove last iteration i.e +O(n)

--- 4_LongestIncreasingSubsequence.java
+++ 4_LongestIncreasingSubsequence.java
@@ -16,18 +16,21 @@ public:
         /*dp.resize(len);
         for(int i=0;i<len;i++)
             dp[i]=1;*/
+        int maxm=dp[0]; //For Kadane's Algorithm
         for(int i=1;i<len;i++){
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
                     dp[i]=max(dp[i], dp[j]+1);
+                    maxm=max(maxm, dp[i]); //Kadane's Algorithm
                 }
             }
         }
-        int max=dp[0];
+        //Not required after using Kadane's Algorithm
+        /*int max=dp[0];
         for(int i=1;i<len;i++){
             if(dp[i]>max)
                 max=dp[i];
-        }
-        return max;
+        }*/
+        return maxm;
     }
 };
