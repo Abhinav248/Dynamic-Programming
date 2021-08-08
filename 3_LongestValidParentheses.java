d38a2bd47bdc3a764643a0368188f9927d5133bd
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-08-09 01:58:19 +0530

    Create 3_LongestValidParentheses.java
    https://leetcode.com/problems/longest-valid-parentheses/ --> 32. Longest Valid Parentheses

---
+++ 3_LongestValidParentheses.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-valid-parentheses/
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int l=s.length();
+        int[] v=new int[l];
+        //v[0]=0;
+        for(int i=1;i<l;i++){
+            if(s.charAt(i)=='(')
+                v[i]=0;
+            else if(s.charAt(i-1)=='(') {
+                v[i]=2;
+                if(i>=2){
+                    v[i]+=v[i-2];
+                }
+            }
+            else if(s.charAt(i-1)==')' && (i-v[i-1]-1 >= 0) && s.charAt(i-v[i-1]-1)=='('){
+                v[i]=v[i-1] + 2;
+                if((i-v[i-1]-2) >= 0){
+                    v[i]+=v[i-v[i-1]-2];
+                }
+            }
+        }
+        int max=0;
+        for(int i=0;i<l;i++){
+            if(v[i]>max)
+                max=v[i];
+        }
+        return max;
+    }
+}
