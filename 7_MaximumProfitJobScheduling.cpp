0a82f595d7c8f308625b8407174462fcc0719152
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-08-11 02:29:32 +0530

    Create 7_MaximumProfitJobScheduling.cpp
    https://leetcode.com/problems/maximum-profit-in-job-scheduling/ -> 1235. Maximum Profit in Job Scheduling
    https://www.youtube.com/watch?v=cr6Ip0J9izc 
    Note: To be optimized.

---
+++ 7_MaximumProfitJobScheduling.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+// https://www.youtube.com/watch?v=cr6Ip0J9izc
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int len=profit.size();
+        vector<int> dp;
+        for(int i=1;i<len;i++){
+            for(int j=0;j<i;j++){
+                if(endTime[j]>endTime[i]){
+                    swap(endTime[j],endTime[i]);
+                    swap(startTime[j],startTime[i]);
+                    swap(profit[j],profit[i]);
+                }
+            }
+        }
+        dp = profit;
+        int maxm=dp[0];
+        for(int i=1;i<len;i++){
+            for(int j=0;j<i;j++){
+                if(startTime[i]>=endTime[j])
+                    dp[i]=max(dp[i],dp[j]+profit[i]);
+                maxm=max(maxm,dp[i]);
+            }
+        }
+        return maxm;
+    }
+};
