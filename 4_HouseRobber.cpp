f56474c369ca8ff21f52116b2d408f5bb038d673
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-08-16 22:11:34 +0530

    Create 4_HouseRobber.cpp
    https://leetcode.com/problems/house-robber/ --> 198. House Robber

---
+++ 4_HouseRobber.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/house-robber/
+// https://www.youtube.com/watch?v=UtGtF6nc35g
+
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp;
+        int len=nums.size();
+        dp.resize(len);
+        dp[0]=nums[0];
+        for(int i=1;i<len;i++){
+            if(i==1)
+                dp[i]=max(nums[0], nums[1]);
+            else
+                dp[i]=max(max(nums[i]+dp[i-2],dp[i-1]),nums[i]);
+        }
+        int maxm=dp[0];
+        for(int i=1;i<len;i++)
+            maxm=max(dp[i],maxm);
+        return maxm;
+    }
+};
+
+/*
+// Java Solution
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        for(int i=1;i<len;i++){
+            if(i==1)
+                dp[i]=Math.max(nums[0],nums[1]);
+            else
+                dp[i]=Math.max(Math.max(nums[i]+dp[i-2],dp[i-1]),nums[i]);
+        }
+        int maxm=dp[0];
+        for(int i=1;i<len;i++)
+            maxm=Math.max(dp[i], maxm);
+        return maxm;
+    }
+}
+*/
