bd9d61f108842735313d444f1a3ac0325697ecb8
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-09-01 18:17:40 +0530

    Update 4-B-PalindromePartitioningMinCount.java

--- 4-B-PalindromePartitioningMinCount.java
+++ 4-B-PalindromePartitioningMinCount.java
@@ -11,20 +11,23 @@ class Solution {
         
         for(int d=0;d<l;d++) {
             for(int i=0, j=d; j<l; i++, j++) {
-                if (d==0) dp[i][j]=true;
-                else if(d==1) {
+                if (d==0) {                                                               // length=1 or (i==j) i.e diagonal
+                    dp[i][j]=true;
+                }
+                else if(d==1) {                                                           // length=2 or j==i+1
                     if(s.charAt(i)==s.charAt(j)) dp[i][j]=true;
                     else count_dp[i][j]=1;
                 } 
-                else if(d>=2){
+                else if(d>=2){                                                            // length>2 or j>=i+2
                     if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]) {
                         dp[i][j]=true;
                         count_dp[i][j]=0;
                     }
                     else {
-                        dp[i][j]=false;
                         int minm=2001;
-                        for(int k=i;k<j;k++) minm=Math.min(minm, count_dp[i][k]+count_dp[k+1][j]);
+                        for(int k=i;k<j;k++) 
+                            minm=Math.min(minm, count_dp[i][k]+count_dp[k+1][j]);
+                        dp[i][j]=false;
                         count_dp[i][j]=minm+1;
                     }
                 }
