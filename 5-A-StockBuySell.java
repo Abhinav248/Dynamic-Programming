ab3c33be569334fa98d846d2f6c5728e3f144de5
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-09-05 02:28:36 +0530

    Update and rename 5-A-StockLimitedTransactionBuySell.java to 5-A-StockBuySell.java
    Added Similar Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ --> 121. Best Time to Buy and Sell Stock

---
+++ 5-A-StockBuySell.java
@@ -0,0 +1,50 @@
+// Related Problemin C++:
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// Approach 1: Time Limit Succeeded
+int maxProfit(vector<int>& prices) {
+    int n=prices.size();
+    int maxm=0;
+    for(int i=0;i<n-1;i++) {
+        for(int j=i+1;j<n;j++)
+            maxm=max(maxm,prices[j]-prices[i]);
+    }
+    return maxm;
+}
+
+// Approach 2: All testcase passed
+int maxProfit(vector<int>& prices) {
+    int len = prices.size();
+    int global_maxProfit = 0;
+    int minPrice_tillNow = prices[0];
+    for (int i = 1; i < len; i++) {
+        int currentProfit = prices[i] - minPrice_tillNow;
+        global_maxProfit = max(global_maxProfit, currentProfit);
+        minPrice_tillNow = min(minPrice_tillNow, prices[i]);
+    }
+    return global_maxProfit;
+}
+
+
+
+// Actual Problem in Java:
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+// Hint: https://www.youtube.com/watch?v=oDhu5uGq_ic
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        if(n<1) return 0;
+        int[][] dp = new int[k+1][n];
+        for(int i=1;i<=k;i++) {
+            for(int j=1;j<n;j++) {
+                int max=0;
+                for (int m=0;m<j;m++) {
+                    max=Math.max(max, prices[j]-prices[m]+dp[i-1][m]);
+                }
+                dp[i][j]=Math.max(dp[i][j-1], max);
+            }
+        }
+        return dp[k][n-1];
+    }
+}
