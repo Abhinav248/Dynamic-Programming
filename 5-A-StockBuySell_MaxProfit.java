ddd54cc9eb24ef4ecb94c66207113b58510aedea
Abhinav <34811385+Abhinav248@users.noreply.github.com>
2021-10-13 23:50:22 +0530

    Update 5-A-StockBuySell_MaxProfit.java

--- 5-A-StockBuySell_MaxProfit.java
+++ 5-A-StockBuySell_MaxProfit.java
@@ -39,10 +39,15 @@ class Solution {
         for(int i=1;i<=k;i++) {
             for(int j=1;j<n;j++) {
                 int max=0;
+                // Here we find out the maximum profit that can be done by selling the stock at jth day and buying it on mth day
                 for (int m=0;m<j;m++) {
-                    max=Math.max(max, prices[j]-prices[m]+dp[i-1][m]);
+                    max=Math.max(max,                  // max profit might be possible for any previous value of m on the below iteration.
+                                 prices[j]-prices[m] + // profit by selling at jth day and buying at mth day where 0<=m<j
+                                 dp[i-1][m]);          // Since we have done 1 transaction above so we are left with (i-1) transaction now and 
+                                 // hence we add the max profit with (i-1) transaction till mth day i.e. dp[i-1][m]
                 }
-                dp[i][j]=Math.max(dp[i][j-1], max);
+                dp[i][j]=Math.max(dp[i][j-1],          // By not transacting on jth day i.e. max profit till (j-1)th day with i transaction = dp[i][j-1]
+                                  max);                // max profit by making a transaction on jth day calculated using above loop logic using m.
             }
         }
         return dp[k][n-1];
